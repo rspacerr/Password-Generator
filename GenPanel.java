e744ff23d5ba592dc82ae870c94d9e22ff33e827
rspacerr <definitelynotoberon@gmail.com>
2021-08-06 23:38:01 -0400

    Changed buttons

--- GenPanel.java
+++ GenPanel.java
@@ -10,7 +10,6 @@ public class GenPanel implements ActionListener {
     private JTextField pwlength;
     private JTextField pwnum;
     private JButton updateLength;
-    private JButton updateNum;
 
     public GenPanel() {
         frame = new JFrame();
@@ -20,31 +19,24 @@ public class GenPanel implements ActionListener {
         pwlength = new JTextField();
         pwnum = new JTextField();
         updateLength = new JButton("Update");
-        updateNum = new JButton("Update");
 
         /* Setup labels */
         panel.setLayout(null);
-        labelLength.setBounds(50, 20, 150, 25);
-        labelNum.setBounds(350, 20, 150, 25);
+        labelLength.setBounds(110, 20, 150, 25);
         panel.add(labelLength);
         panel.add(labelNum);
 
         /* Setup Textfields */
-        pwlength.setBounds(65, 40, 80, 25);
-        pwnum.setBounds(385, 40, 80, 25);
+        pwlength.setBounds(120, 40, 80, 25);
         panel.add(pwlength);
         panel.add(pwnum);
 
         /* Setup Buttons */
-        updateLength.setBounds(65, 65, 80, 25);
-        updateNum.setBounds(385, 65, 80, 25);
-        updateLength.addActionListener(new GenPanel());
-        updateLength.addActionListener(new GenPanel());
+        updateLength.setBounds(120, 65, 80, 25);
         panel.add(updateLength);
-        panel.add(updateNum);
 
         frame.add(panel, BorderLayout.CENTER);
-        frame.setSize(550, 400);
+        frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Password Generator");
         frame.setVisible(true);
@@ -54,7 +46,6 @@ public class GenPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO: implement save text
-        
     }
 
     public static void main(String[] args) {
