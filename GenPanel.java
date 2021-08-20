680893f3a5d0933002e737c57dfbfee0aed8f004
rspacerr <definitelynotoberon@gmail.com>
2021-08-20 18:01:58 -0400

    Added password creation functionality

--- GenPanel.java
+++ GenPanel.java
@@ -3,37 +3,43 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GenPanel implements ActionListener {
-    private JFrame frame;
-    private JPanel panel;
-    private JLabel labelLength;
-    private JLabel labelNum;
-    private JTextField pwlength;
-    private JTextField pwnum;
-    private JButton updateLength;
+    private static JFrame frame;
+    private static JPanel panel;
+    private static JLabel labelLength;
+    // private JLabel labelNum;
+    private static JTextField pwlength;
+    // private static JTextField pwnum;
+    private static JButton generate;
+    private static int passwordLength;
+    private static JLabel passwordLabel;
 
     public GenPanel() {
         frame = new JFrame();
         panel = new JPanel();
         labelLength = new JLabel("Password length:");
-        labelNum = new JLabel("Number of passwords:");
+        // labelNum = new JLabel("Number of passwords:");
+        // pwnum = new JTextField();
         pwlength = new JTextField();
-        pwnum = new JTextField();
-        updateLength = new JButton("Update");
+        passwordLabel = new JLabel("");
+        generate = new JButton("Generate");
 
         /* Setup labels */
         panel.setLayout(null);
         labelLength.setBounds(110, 20, 150, 25);
+        passwordLabel.setBounds(100, 95, 200, 25);
         panel.add(labelLength);
-        panel.add(labelNum);
+        panel.add(passwordLabel);
+        // panel.add(labelNum);
 
         /* Setup Textfields */
         pwlength.setBounds(120, 40, 80, 25);
         panel.add(pwlength);
-        panel.add(pwnum);
+        // panel.add(pwnum);
 
         /* Setup Buttons */
-        updateLength.setBounds(120, 65, 80, 25);
-        panel.add(updateLength);
+        generate.setBounds(120, 65, 80, 25);
+        generate.addActionListener(this);
+        panel.add(generate);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setSize(300, 200);
@@ -45,7 +51,22 @@ public class GenPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO: implement save text
+        try {
+            passwordLength = Integer.parseInt(pwlength.getText());
+            passwordLabel.setText(generatePasswords(passwordLength));
+        }
+        catch (NumberFormatException n) {
+            passwordLabel.setText("Enter a length!");
+        }
+    }
+
+    public String generatePasswords(int length) {
+        java.util.Random r = new java.util.Random();
+        String password = "";
+        for (int i = 0; i < length; i++) {
+            password += ((char) ('!' + r.nextInt(93)));
+        }
+        return password;
     }
 
     public static void main(String[] args) {
